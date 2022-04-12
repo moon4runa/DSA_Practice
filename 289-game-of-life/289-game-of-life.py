ceb975420432f0a052f503fadedc122152da68a7
Ruchi Jain <49330032+moon4runa@users.noreply.github.com>
2022-04-12 12:32:16 +0530

    Time: 65 ms (14.32%), Space: 14 MB (51.25%) - LeetHub

--- 289-game-of-life/289-game-of-life.py
+++ 289-game-of-life/289-game-of-life.py
@@ -3,6 +3,13 @@ class Solution:
         """
         Do not return anything, modify board in-place instead.
         """
+        # Updated
+        # 0 -> Not updated
+        # 1 -> Not updated
+        # 2 -> Updated(before it was zero)
+        # 3 -> Updated(befoer it was one)
+        
+        
         res = []
         m = len(board)
         n = len(board[0])
@@ -12,43 +19,46 @@ class Solution:
             for j in range(len(board[0])):
                 c = 0
                 if i+1 < m:
-                    if board[i+1][j] == 1:
+                    if board[i+1][j] == 1 or board[i+1][j] == 3:
                         c += 1
                     if j+1 < n:
-                        if board[i+1][j+1] == 1:
+                        if board[i+1][j+1] == 1 or board[i+1][j+1] == 3:
                             c += 1
                     if j-1 >=0:
-                        if board[i+1][j-1] == 1:
+                        if board[i+1][j-1] == 1 or board[i+1][j-1] == 3:
                             c += 1
                 if i-1 >= 0:
-                    if board[i-1][j] == 1:
+                    if board[i-1][j] == 1 or board[i-1][j] == 3:
                         c += 1
                     if j-1 >= 0:
-                        if board[i-1][j-1] == 1:
+                        if board[i-1][j-1] == 1 or board[i-1][j-1] == 3:
                             c += 1
                     if j+1 < n:
-                        if board[i-1][j+1] == 1:
+                        if board[i-1][j+1] == 1 or board[i-1][j+1] == 3:
                             c += 1 
                 if j+1 < n:
-                    if board[i][j+1] == 1:
+                    if board[i][j+1] == 1 or board[i][j+1] == 3:
                         c += 1
                 if j-1 >= 0:
-                    if board[i][j-1] == 1:
+                    if board[i][j-1] == 1 or board[i][j-1] == 3:
                         c+=1
                 
                 if board[i][j] == 1:
                     if c < 2:
-                        res[i][j] = 0
+                        board[i][j] = 3
                     if c == 2 or c == 3:
-                        res[i][j] = 1 
+                        board[i][j] = 1
                     if c > 3:
-                        res[i][j] = 0 
+                        board[i][j] = 3 
                 else:
                     if c == 3:
-                        res[i][j] = 1
+                        board[i][j] = 2
         
         for i in range(len(board)):
             for j in range(len(board[0])):
-                board[i][j] = res[i][j]
+                if board[i][j] == 3:
+                    board[i][j] = 0 
+                if board[i][j] == 2:
+                    board[i][j] = 1
             
                     
