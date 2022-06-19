fbf8fce70bddfaf536d83c4d3bb01c8d2d0bc256
Ruchi Jain <49330032+moon4runa@users.noreply.github.com>
2022-06-19 18:17:38 +0530

    Added solution - LeetHub

--- Stack using two queues - GFG/stack-using-two-queues.cpp
+++ Stack using two queues - GFG/stack-using-two-queues.cpp
@@ -55,30 +55,46 @@ public:
 //Function to push an element into stack using two queues.
 void QueueStack :: push(int x)
 {
-      if(q1.empty()) q1.push(x);
-      else{
-          while(!q1.empty()){
-              q2.push(q1.front());
-              q1.pop();
-          }
-          q1.push(x);
-          while(!q2.empty()){
-              q1.push(q2.front());
-              q2.pop();
-          }
+    q1.push(x);
+    //   if(q1.empty()) q1.push(x);
+    //   else{
+    //       while(!q1.empty()){
+    //           q2.push(q1.front());
+    //           q1.pop();
+    //       }
+    //       q1.push(x);
+    //       while(!q2.empty()){
+    //           q1.push(q2.front());
+    //           q2.pop();
+    //       }
           
-      }
+    //   }
         // Your Code
 }
 
 //Function to pop an element from stack using two queues. 
 int QueueStack :: pop()
 {
+    // if(q1.empty()) return -1;
+    // else{
+    //     int curr = q1.front();
+    //     q1.pop();
+    //     return curr;
+    // }
+    
     if(q1.empty()) return -1;
-    else{
-        int curr = q1.front();
+    while(q1.size() != 1){
+        q2.push(q1.front());
         q1.pop();
-        return curr;
     }
+    int curr = q1.front();
+    q1.pop();
+    while(!q2.empty()){
+        q1.push(q2.front());
+        q2.pop();
+    }
+    return curr;
+    
+    
         // Your Code       
 }
