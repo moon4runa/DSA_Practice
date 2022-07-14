3f9d08504c87f5ae11ef3b9fb7e9121e5bdf6070
Ruchi Jain <49330032+moon4runa@users.noreply.github.com>
2022-07-14 14:40:21 +0530

    Time: 6 ms (7.86%), Space: 5.9 MB (65.85%) - LeetHub

---
+++ 96-unique-binary-search-trees/96-unique-binary-search-trees.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int numTrees(int n) {
+        if(n==1) return 1;
+        vector<int> dp(n+1);
+        for(int i=0; i<=n; i++){
+            if(i<2) dp[i] = 1;
+            else{
+                for(int j = 0; j<i; j++){
+                    dp[i] += dp[j]*dp[i-j-1]; 
+                }
+            }
+        }
+        return dp[n];
+    }
+};
