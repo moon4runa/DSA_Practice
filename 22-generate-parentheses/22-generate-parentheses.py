b860282ee78793aca5b7e137bc0db4fb67773c2a
Ruchi Jain <49330032+moon4runa@users.noreply.github.com>
2022-07-14 15:03:07 +0530

    Time: 65 ms (27.85%), Space: 14.3 MB (12.27%) - LeetHub

--- 22-generate-parentheses/22-generate-parentheses.py
+++ 22-generate-parentheses/22-generate-parentheses.py
@@ -1,18 +1,19 @@
-class Solution {
-public:
-    void solve(int n, vector<string> &ans, int co, int cc, string temp){
-        if(co==cc && temp.length() == 2 * n){
-            ans.push_back(temp);
-            return;
-        }
-        if(co < n) solve(n, ans, co+1, cc, temp+"(");
-        if(cc < co && cc < n) solve(n, ans, co, cc+1, temp+")");
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        # Dynamic Programming
+        dp = [];
+        dp.append(set(["()"]))
+        if n==1:
+            return dp[0]
+        for i in range(1, n):
+            temp_set = set()
+            for prev in dp[i-1]:
+                for j in range(0, len(prev)):
+                    temp_set.add(prev[0:j] + "()" + prev[j:])
+            dp.append(temp_set)
+        return dp[n-1]
+                    
+                        
+                    
+                
         
-    }
-    
-    vector<string> generateParenthesis(int n) {
-        vector<string> ans;
-        solve(n, ans, 0,0,"");
-        return ans;
-    }
-};
