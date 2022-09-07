aa41f0498f2cbb3900f6a8f53831ac795d9c783c
Ruchi Jain <49330032+moon4runa@users.noreply.github.com>
2022-09-07 23:11:36 +0530

    Time: 614 ms (61.79%), Space: 13.9 MB (98.24%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.py
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.py
@@ -15,23 +15,36 @@ class Solution:
     
         n = len(text1)
         m = len(text2)
-        dp = []
-        for i in range(n+1):
-            dp.append([-1]*(m+1))
- 
+        
+        prev = [0]*(m+1)
         
         
-        
-        for ind1 in range(n+1):
-            for ind2 in range(m+1):
-                if(ind1 == 0 or ind2 == 0):
-                    dp[ind1][ind2] = 0
-                elif text1[ind1 -1]==text2[ind2-1]:
-                    dp[ind1][ind2] = 1 + dp[ind1-1][ind2-1]
+        for ind1 in range(1, n+1):
+            curr = [0]*(m+1)
+            for ind2 in range(1, m+1):
+                if text1[ind1 -1]==text2[ind2-1]:
+                    curr[ind2] = 1 + prev[ind2-1]
                 else:
-                    dp[ind1][ind2] = max(dp[ind1-1][ind2], dp[ind1][ind2-1])
+                    curr[ind2] = max(prev[ind2], curr[ind2-1])
+            prev = curr[:]
+        return curr[m]
                 
-        return dp[n][m]
+        
+# Tabulation        
+#         dp = []
+#         for i in range(n+1):
+#             dp.append([-1]*(m+1))
+        
+#         for ind1 in range(n+1):
+#             for ind2 in range(m+1):
+#                 if(ind1 == 0 or ind2 == 0):
+#                     dp[ind1][ind2] = 0
+#                 elif text1[ind1 -1]==text2[ind2-1]:
+#                     dp[ind1][ind2] = 1 + dp[ind1-1][ind2-1]
+#                 else:
+#                     dp[ind1][ind2] = max(dp[ind1-1][ind2], dp[ind1][ind2-1])
+                
+#         return dp[n][m]
             
         
         # return lcs(text1, text2, len(text1)-1, len(text2)-1, dp)
