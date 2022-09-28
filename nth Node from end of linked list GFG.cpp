46d01611adc4716e0c339c6f298ed734bcc58eb2
Ruchi Jain <49330032+moon4runa@users.noreply.github.com>
2022-09-28 12:43:49 +0530

    Create nth Node from end of linked list GFG.cpp

---
+++ nth Node from end of linked list GFG.cpp
@@ -0,0 +1,90 @@
+//{ Driver Code Starts
+// C program to find n'th Node in linked list
+#include <stdio.h>
+#include <stdlib.h>
+#include<iostream>
+using namespace std;
+
+/* Link list Node */
+struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+};
+
+
+/* Function to get the nth node from the last of a linked list*/
+int getNthFromLast(struct Node* head, int n);
+
+
+
+/* Driver program to test above function*/
+int main()
+{
+  int T,i,n,l,k;
+
+    cin>>T;
+
+    while(T--){
+    struct Node *head = NULL,  *tail = NULL;
+
+        cin>>n>>k;
+        int firstdata;
+        cin>>firstdata;
+        head = new Node(firstdata);
+        tail = head;
+        for(i=1;i<n;i++)
+        {
+            cin>>l;
+            tail->next = new Node(l);
+            tail = tail->next;
+        }
+
+    cout<<getNthFromLast(head, k)<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
+
+
+/* struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+};
+*/
+
+//Function to find the data of nth node from the end of a linked list.
+int getNthFromLast(Node *head, int n)
+{
+    
+      Node* dummy = new Node(0);
+      dummy->next = head;
+      Node* fast = dummy;
+      Node* slow = head;
+      
+      for(int i=0; i<n; i++){
+          fast = fast->next;
+          if(i<n && fast==NULL) return -1;
+      }
+      
+      while(fast->next!=NULL){
+          fast= fast->next;
+          slow = slow->next;
+      }
+      return slow->data;
+      
+      
+      
+      
+      
+      
+       // Your code here
+}
+
