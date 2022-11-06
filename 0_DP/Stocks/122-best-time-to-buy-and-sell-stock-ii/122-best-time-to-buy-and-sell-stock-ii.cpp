ecea6e2a096d6204c9034060e4787b55555d5e47
Ruchi Jain <49330032+moon4runa@users.noreply.github.com>
2022-11-06 09:54:48 +0530

    Update 122-best-time-to-buy-and-sell-stock-ii.cpp

--- 0_DP/Stocks/122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0_DP/Stocks/122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -5,6 +5,15 @@ public:
     
     int maxProfit(vector<int>& prices) {
         
+        // Greedy
+        //  int maxProfit(vector<int>& prices) {
+//             int profit = 0;
+            
+//             for(int i=1; i<prices.size(); i++){
+//                 if(prices[i] > prices[i-1]) profit += (prices[i] - prices[i-1]);
+//             }
+//             return profit;
+        // }
         
         // Space Optimization
         int n = prices.size();
